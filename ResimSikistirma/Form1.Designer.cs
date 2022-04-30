ffdb302b10e50cd0fd7721b59288aaa090515e03
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2022-05-01 00:32:51 +0300

    Tamamlandı

---
+++ ResimSikistirma/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ResimSikistirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aquaGauge1 = new AquaControls.AquaGauge();
+            this.Sikistir_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ResimSec_button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CCITT4_radioButton = new System.Windows.Forms.RadioButton();
+            this.CCITT3_radioButton = new System.Windows.Forms.RadioButton();
+            this.LZW_radioButton = new System.Windows.Forms.RadioButton();
+            this.RLE_radioButton = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ResimAd_label = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // aquaGauge1
+            // 
+            this.aquaGauge1.BackColor = System.Drawing.Color.Transparent;
+            this.aquaGauge1.DialColor = System.Drawing.Color.Lavender;
+            this.aquaGauge1.DialText = null;
+            this.aquaGauge1.Glossiness = 11.36364F;
+            this.aquaGauge1.Location = new System.Drawing.Point(12, 276);
+            this.aquaGauge1.MaxValue = 100F;
+            this.aquaGauge1.MinValue = 0F;
+            this.aquaGauge1.Name = "aquaGauge1";
+            this.aquaGauge1.RecommendedValue = 0F;
+            this.aquaGauge1.Size = new System.Drawing.Size(150, 150);
+            this.aquaGauge1.TabIndex = 0;
+            this.aquaGauge1.ThresholdPercent = 1F;
+            this.aquaGauge1.Value = 0F;
+            // 
+            // Sikistir_button
+            // 
+            this.Sikistir_button.Location = new System.Drawing.Point(6, 153);
+            this.Sikistir_button.Name = "Sikistir_button";
+            this.Sikistir_button.Size = new System.Drawing.Size(138, 40);
+            this.Sikistir_button.TabIndex = 1;
+            this.Sikistir_button.Text = "SIKIŞTIR";
+            this.Sikistir_button.UseVisualStyleBackColor = true;
+            this.Sikistir_button.Click += new System.EventHandler(this.Sikistir_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ResimSec_button
+            // 
+            this.ResimSec_button.Location = new System.Drawing.Point(12, 12);
+            this.ResimSec_button.Name = "ResimSec_button";
+            this.ResimSec_button.Size = new System.Drawing.Size(150, 40);
+            this.ResimSec_button.TabIndex = 2;
+            this.ResimSec_button.Text = "Resim Seç";
+            this.ResimSec_button.UseVisualStyleBackColor = true;
+            this.ResimSec_button.Click += new System.EventHandler(this.ResimSec_button_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CCITT4_radioButton);
+            this.groupBox1.Controls.Add(this.CCITT3_radioButton);
+            this.groupBox1.Controls.Add(this.LZW_radioButton);
+            this.groupBox1.Controls.Add(this.RLE_radioButton);
+            this.groupBox1.Controls.Add(this.Sikistir_button);
+            this.groupBox1.Location = new System.Drawing.Point(12, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 202);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sıkıştırma Türü";
+            // 
+            // CCITT4_radioButton
+            // 
+            this.CCITT4_radioButton.AutoSize = true;
+            this.CCITT4_radioButton.Location = new System.Drawing.Point(28, 116);
+            this.CCITT4_radioButton.Name = "CCITT4_radioButton";
+            this.CCITT4_radioButton.Size = new System.Drawing.Size(65, 17);
+            this.CCITT4_radioButton.TabIndex = 5;
+            this.CCITT4_radioButton.TabStop = true;
+            this.CCITT4_radioButton.Text = "CCITT-4";
+            this.CCITT4_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // CCITT3_radioButton
+            // 
+            this.CCITT3_radioButton.AutoSize = true;
+            this.CCITT3_radioButton.Location = new System.Drawing.Point(28, 87);
+            this.CCITT3_radioButton.Name = "CCITT3_radioButton";
+            this.CCITT3_radioButton.Size = new System.Drawing.Size(65, 17);
+            this.CCITT3_radioButton.TabIndex = 4;
+            this.CCITT3_radioButton.TabStop = true;
+            this.CCITT3_radioButton.Text = "CCITT-3";
+            this.CCITT3_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // LZW_radioButton
+            // 
+            this.LZW_radioButton.AutoSize = true;
+            this.LZW_radioButton.Location = new System.Drawing.Point(28, 59);
+            this.LZW_radioButton.Name = "LZW_radioButton";
+            this.LZW_radioButton.Size = new System.Drawing.Size(49, 17);
+            this.LZW_radioButton.TabIndex = 3;
+            this.LZW_radioButton.TabStop = true;
+            this.LZW_radioButton.Text = "LZW";
+            this.LZW_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // RLE_radioButton
+            // 
+            this.RLE_radioButton.AutoSize = true;
+            this.RLE_radioButton.Location = new System.Drawing.Point(28, 30);
+            this.RLE_radioButton.Name = "RLE_radioButton";
+            this.RLE_radioButton.Size = new System.Drawing.Size(46, 17);
+            this.RLE_radioButton.TabIndex = 2;
+            this.RLE_radioButton.TabStop = true;
+            this.RLE_radioButton.Text = "RLE";
+            this.RLE_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(168, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 410);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ResimAd_label
+            // 
+            this.ResimAd_label.AutoSize = true;
+            this.ResimAd_label.ForeColor = System.Drawing.Color.Red;
+            this.ResimAd_label.Location = new System.Drawing.Point(168, 418);
+            this.ResimAd_label.Name = "ResimAd_label";
+            this.ResimAd_label.Size = new System.Drawing.Size(91, 13);
+            this.ResimAd_label.TabIndex = 5;
+            this.ResimAd_label.Text = "Resim seçilmedi...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(617, 435);
+            this.Controls.Add(this.ResimAd_label);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ResimSec_button);
+            this.Controls.Add(this.aquaGauge1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Resim Sıkıştırma Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AquaControls.AquaGauge aquaGauge1;
+        private System.Windows.Forms.Button Sikistir_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button ResimSec_button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton CCITT4_radioButton;
+        private System.Windows.Forms.RadioButton CCITT3_radioButton;
+        private System.Windows.Forms.RadioButton LZW_radioButton;
+        private System.Windows.Forms.RadioButton RLE_radioButton;
+        private System.Windows.Forms.Label ResimAd_label;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ResimSikistirma/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ResimSikistirma
+{
+    public partial class Form1 : Form
+    {
+
+        private long dosyaBoyutu1, dosyaBoyutu2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RLE_radioButton.Checked = true;
+        }
+
+        ImageCodecInfo bul(string MimeType)
+        {
+            int i;
+            for (i = 0; i <= ImageCodecInfo.GetImageEncoders().Length - 1; i++)
+                if (ImageCodecInfo.GetImageEncoders()[i].MimeType == MimeType)
+                {
+                    return ImageCodecInfo.GetImageEncoders()[i];
+                }
+            return null;
+        }
+
+        private void ResimSec_button_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Resim dosyaları | *.bmp; *.jpg; *.gif; *.wmf; *.tif; *.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                    FileInfo info = new FileInfo(openFileDialog1.FileName);
+	                dosyaBoyutu1 = info.Length;
+                    ResimAd_label.Text = openFileDialog1.FileName;
+
+                }
+                catch
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "Geçerli resim dosyası değil");
+                }
+        }
+
+        private void Sikistir_button_Click(object sender, EventArgs e)
+        {
+            
+            //BMP Kaydet
+            saveFileDialog1.Filter = "Bmp Dosyası | *.bmp";
+            saveFileDialog1.DefaultExt = "bmp";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Image resim;
+                resim = pictureBox1.Image;
+                ImageCodecInfo Codec_Bilgisi;
+                //bmp encoderini bul
+                Codec_Bilgisi = bul("image/bmp");
+                EncoderParameter parametre;
+                //sıkıştırma
+                if (LZW_radioButton.Checked)
+                {
+                    parametre = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (int)EncoderValue.CompressionLZW);
+                }
+                if (CCITT3_radioButton.Checked)
+                {
+                    parametre = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (int)EncoderValue.CompressionCCITT3);
+                }
+                if (CCITT4_radioButton.Checked)
+                {
+                    parametre = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (int)EncoderValue.CompressionCCITT4);
+                }
+
+                else
+                    parametre = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (int)EncoderValue.CompressionRle);
+               
+                
+                EncoderParameters parametreler;
+                //1 parametremiz var
+                parametreler = new EncoderParameters(1);
+                //parametreleri ata
+                parametreler.Param[0] = parametre;
+                //kaydet
+
+
+                 resim.Save(saveFileDialog1.FileName, Codec_Bilgisi, parametreler);
+
+                 FileInfo info2 = new FileInfo(saveFileDialog1.FileName);
+	             dosyaBoyutu2 = info2.Length;
+
+                 if (dosyaBoyutu1 > dosyaBoyutu2)
+                 {
+                    int sikistirmaOrani = Convert.ToInt32(dosyaBoyutu2 / dosyaBoyutu1) * 100;
+                    aquaGauge1.Value = Convert.ToInt16(sikistirmaOrani) ;
+
+                   
+                    MessageBox.Show("sıkıştırma oranı:" +sikistirmaOrani.ToString());
+                 }
+
+                 if (dosyaBoyutu1 <= dosyaBoyutu2)
+                 {
+                     MessageBox.Show("Resim sıkıştırma başarısız!");
+                 }
+
+
+                
+            } 
+           
+        }
+    }
+}
